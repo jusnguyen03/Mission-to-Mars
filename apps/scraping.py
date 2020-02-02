7b191d2e742a5f885f12ba15b89912d2eb0e6bba
Justin Nguyen <tan.nguyen03@gmail.com>
2020-02-02 13:30:10 -0800

    Module 10.6.1 Complete

--- apps/scraping.py
+++ apps/scraping.py
@@ -6,9 +6,12 @@ import datetime as dt
 
 # Set the executable path and initialize the chrome browser in splinter
 executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
+#browser = Browser('chrome', **executable_path, headless=False)
 
 def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser('chrome', **executable_path, headless=True)
+    
     news_title, news_paragraph = mars_news(browser)
     # Run all scraping functions and store results in dictionary
     data = {
@@ -18,8 +21,9 @@ def scrape_all():
     "facts": mars_facts(),
     "last_modified": dt.datetime.now()
     }
-    return(data)
+    return data
 
+# Visit the NASA mars news site
 def mars_news(browser):
 
     # Visit the mars nasa news site
@@ -35,45 +39,43 @@ def mars_news(browser):
     # Add try/except for error handling
     try:
         slide_elem = news_soup.select_one('ul.item_list li.slide')
-        # Use the parent element to find the first `a` tag and save it as `news_title`
+        # Use the parent element to find the first 'a' tag and save it as 'new_title'
         news_title = slide_elem.find("div", class_='content_title').get_text()
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-        #news_p
 
-        #return news_title, news_p
     except AttributeError:
         return None, None
     return news_title, news_p
 
 def featured_image(browser):
+    # Featured Images
+
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+
+    # Find the more info button and click that
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+
     try:
-        # Visit URL
-        url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-        browser.visit(url)
-
-        # Find and click the full image button
-        full_image_elem = browser.find_by_id('full_image')
-        full_image_elem.click()
-
-        # Find the more info button and click that
-        browser.is_element_present_by_text('more info', wait_time=1)
-        more_info_elem = browser.find_link_by_partial_text('more info')
-        more_info_elem.click()
-
-        # Parse the resulting html with soup
-        html = browser.html
-        img_soup = BeautifulSoup(html, 'html.parser')
-
         # Find the relative image url
         img_url_rel = img_soup.select_one('figure.lede a img').get("src")
 
-        # Use the base URL to create an absolute URL
-        img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-        return(img_url)
-
     except AttributeError:
         return None
+    
+    return "https://www.jpl.nasa.gov/"+ img_url_rel
 
 def mars_facts():
     #Add try/exept for error handling
@@ -81,19 +83,14 @@ def mars_facts():
         #Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('http://space-facts.com/mars/')[0]
     
-        # Assign columns and set index of dataframe
-        df.columns=['description', 'Mars', 'Earth']
-        df.set_index('description', inplace=True)
-        # Convert dataframe into HTML format, add bootstrap
-        return(df.to_html())
     except BaseException:
         return None
+    # Assign columns and set index of dataframe
+    df.columns=['description', 'Mars']
+    df.set_index('description', inplace=True)
+    # Convert dataframe into HTML format, add bootstrap
+    return(df.to_html())
 
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
-
-
-
-
-
